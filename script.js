76421cfc80d2ce89be74f585512ef4be4541ebad
David James <cendiin@gmail.com>
2019-06-06 10:40:34 -0400

    Working RPS Project

--- script.js
+++ script.js
@@ -1,18 +1,63 @@
+function resetGame(){
+    if (document.getElementById("computer-score").innerHTML == 5){
+        document.getElementById("results").innerHTML = "The computer wins!"
+    }
+        else {
+            document.getElementById("results").innerHTML = "You win!"
+    }
+    document.getElementById("rock").disabled = true;
+    document.getElementById("paper").disabled = true;
+    document.getElementById("scissors").disabled = true;
+    let button = document.createElement("button");
+    button.innerHTML = "Reset Game";
+    button.id = "reset"
+    button.class = "reset"
+    button.onclick = function(){gameInit()}
+    let results = document.getElementById("results-window");
+    results.appendChild(button)
+}
+
 function computerPlay() {
-    let RPS = pickRandom(["Rock", "Paper", "Scissors"]);
-   return RPS
+    let rps = ["Rock", "Paper", "Scissors"];
+   return rps[Math.floor(Math.random() * rps.length)];
 }
-function playRound(playerSelection, computerSelection) {
+
+function gameInit() {
+    document.getElementById("rock").disabled = false;
+    document.getElementById("paper").disabled = false;
+    document.getElementById("scissors").disabled = false;
+    let resetButton = document.getElementById("reset");
+    resetButton.remove()
+    document.getElementById("player-score").innerHTML = 0
+    document.getElementById("computer-score").innerHTML = 0
+    document.getElementById("results").innerHTML = ""
+    document.getElementById("computer-results").innerHTML = ""
+}
+
+function playRound(playerSelection) {
+    let computerSelection = computerPlay();
+    document.getElementById("computer-results").innerHTML = `The computer chose ${computerSelection}.`;
    if (playerSelection == computerSelection){
-       return "TIE"
+    document.getElementById("results").innerHTML = "Tie"
    }
-   if (computerSelection == "Paper"){
-       return "I WIN"
+   if (computerSelection == "Paper" && playerSelection == "Scissors" || 
+       computerSelection == "Scissors" && playerSelection == "Rock"  ||
+       computerSelection == "Rock" && playerSelection == "Paper"){
+    document.getElementById("results").innerHTML = "You win this round."
+    increaseScore("player-score")
    }
-   if (computerSelection == "Scissors"){
-       return "I LOSE"
+   else {
+    document.getElementById("results").innerHTML = "You lose this round."
+    increaseScore("computer-score")
    }
 }
-const playerSelection = 'Rock'
-const computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection))
+
+function increaseScore(score) { //score is player-score or computer-score
+    var valResult = document.getElementById(score).innerHTML;
+    parseInt(valResult, 10);
+    valResult ++
+    document.getElementById(score).innerHTML = valResult;
+    if (document.getElementById(score).innerHTML == 5) { //Win condition
+       resetGame();
+    }
+}
